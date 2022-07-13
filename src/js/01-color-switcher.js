9555f068d744edd3f525b5ae586bc2a0de7adb02
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-13 20:12:30 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+bodyEl = document.querySelector('body');
+btnStartEl = document.querySelector('[data-start]');
+btnStopEl = document.querySelector('[data-stop]');
+btnStopEl.setAttribute('disabled', 'true');
 
+btnStartEl.addEventListener('click', startSwitchColor);
+btnStopEl.addEventListener('click', stopSwitchColor);
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startSwitchColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(
+    () => (bodyEl.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  btnStartEl.setAttribute('disabled', 'true');
+  btnStopEl.removeAttribute('disabled');
+}
+
+function stopSwitchColor() {
+  btnStartEl.removeAttribute('disabled');
+  btnStopEl.setAttribute('disabled', 'true');
+  clearInterval(timerId);
+}
